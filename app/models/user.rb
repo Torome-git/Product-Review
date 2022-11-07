c55761af2e23bd92a7bfabad533842241a96ed05
soila.torome <soila.torome@student.moringaschool.com>
2022-11-07 19:05:17 +0300

    Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
+
+    def favorite_product
+        self.products.max_by do |product|
+            product.average_rating
+        end
+    end
+
+    def remove_reviews(product)
+        self.reviews.where(product: product).destroy_all
+    end
     
 end
